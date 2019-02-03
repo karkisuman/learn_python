284ec69a673c036b8f1f34737de41bed42d08229
Sumann <karki007suman@gmail.com>
2019-02-03 10:15:43 +0545

    first commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+
+'''mark1=int(input("Entermark 1 \n"))
+mark2=int(input("Enter mark 2\n"))
+mark3=int(input("Entermark 3\n"))
+mark4=int(input("Enter mark 4\n"))
+mark5=int(input("Entermark 5\n"))
+sum=mark1+mark2+mark3+mark4+mark5
+print("sum",sum)'''
+user=['khadga','suman']
+#newUser=input("Enter name:\n")
+#for u in user:
+marks=[]
+sum=0
+marks1=int(input("enter mark1\n"))
+marks.append(marks1)
+marks2=int(input("enter marks2\n"))
+marks.append(marks2)
+marks3=int(input("enter marks3\n"))
+marks.append(marks3)
+for m in marks:
+    sum+=m
+print(sum)
+
